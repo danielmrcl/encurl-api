1bd29510b6bf7ba5346d0fb20605c13bcf1e1341
Daniel Jr <dan@danielmrcl.dev>
2025-04-29 14:46:00 -0300

    Add basic authentication to generate URL
    Signed-off-by: Daniel Jr <dan@danielmrcl.dev>

---
+++ AuthService.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+public class AuthService
+{
+	private readonly string _staticUsername;
+	private readonly string _staticPassword;
+
+	public AuthService(IOptions<StaticCretentialsSettings> credentialOptions)
+	{
+		_staticUsername = credentialOptions.Value.Username;
+		_staticPassword = credentialOptions.Value.Password;
+	}
+
+	public bool IsAuthorized(string token)
+	{
+		if (!token.StartsWith("Basic "))
+		{
+			return false;
+		}
+
+		var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(token.Substring(6)));
+		var username = decoded.Split(":")[0];
+		var password = decoded.Split(":")[1];
+		return username == _staticUsername && password == _staticPassword;
+	}
+}
